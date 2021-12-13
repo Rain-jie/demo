bd76ab96ca0eef8940e9e6b4947657c203ba8d6e
史浩杰 <58285940+Rain-jie@users.noreply.github.com>
2021-12-13 18:06:24 +0800

    Create applincationstop.sh

---
+++ applincationstop.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# func:自动监控tomcat脚本并且执行重启操作
+#tomcat启动程序(这里注意Tomcat实际安装的路径)
+StartTomcat=/usr/local/src/software/tomcat/epictopia-tomacat-8.5.71-8081/bin/startup.sh
+TomcatWorkCache=/usr/local/src/software/tomcat/epictopia-tomacat-8.5.71-8081/work
+TomcatConfCache=/usr/local/src/software/tomcat/epictopia-tomacat-8.5.71-8081/conf
+TomcatWebapps=/usr/local/src/software/tomcat/epictopia-tomacat-8.5.71-8081/webapps
+
+
+#执行脚本是为了方便CodeDeploy部署服务
+#Code Deploy 执行此脚本是为了部署并执行此服务，所以每次执行时我们首先清除服务缓存
+#清理缓存后在启动服务
+
+#第一步 清理旧服务遗留
+rm -rf $TomcatWebapps/epictopia
+rm -rf $TomcatWebapps/epictopia.war
+
+#第二步 清理Tomcat缓存
+rm -rf $TomcatWorkCache/Catalina
+rm -rf $TomcatConfCache/Catalina
+
+#第三步 启动服务
+$StartTomcat
+#/usr/local/src/software/tomcat/epictopia-tomacat-8.5.71-8081/bin/startup.sh
